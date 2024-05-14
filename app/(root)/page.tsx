18534b012a0f33e9124dda591e9e5be549af9e04
mikesila <michaelsila97@gmail.com>
2024-05-14 18:44:30 +0300

    Implement Sidebar and mobile navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
+import Headerbox from "@/components/ui/Headerbox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
+    const loggedIn = { firstName: 'Michael'}
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <Headerbox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your finances and transactions efficiently"
+                    />
+                </header>
+
+                <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.35}
+                />
+            </div>
+        </section>
     )
 }
 
